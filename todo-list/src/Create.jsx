f7039ac0235efc070dc6bc2146e636af56bf4f8a
Lucas Santos <lucsantosdev@gmail.com>
2024-07-03 00:16:16 -0300

    database connection

--- todo-list/src/Create.jsx
+++ todo-list/src/Create.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 function Create() {
+    const [task, setTask] = useState()
+    const handleAdd = () => {
+        axios.post('https://localhost:3001/add', {task: task})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
     return (
-        <div>
-            <input type="text" name="" id="" />
-            <button type="button">Add</button>
+        <div className="create_form">
+            <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)} />
+            <button type="button" onClick={handleAdd}>Add</button>
         </div>
     )
 }
