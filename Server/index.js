18a2b6b3eee7136e692df890355bfdaa93334de4
Lucas Santos <lucsantosdev@gmail.com>
2024-07-04 22:05:13 -0300

    tasks inclusions (w/ styles and main functionalities)

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const TodoModel = require('.Models/Todo')
 
 const app = express()
 app.use(cors())
@@ -8,9 +9,33 @@ app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/todo-list')
 
-app.post('/add', (req, res => {
+app.get('/get', (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.put('/update/:id', (req, res) => {
+    const {id} = req.params
+    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params
+    TodoModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post('/add', (req, res) => {
     const task = req.body.task
-}))
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
 
 app.listen(3001, () => {
     console.log("Server is running.")
